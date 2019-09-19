750e9b3fc12fd1c2e7f94d1001c70421ae5a72b4
miky <lindsey.m.hughes+teaching@gmail.com>
2019-09-18 17:19:28 -0700

    show baby count

--- app/src/main/java/com/linz/miky/cpsc356/MainActivity.kt
+++ app/src/main/java/com/linz/miky/cpsc356/MainActivity.kt
@@ -1,30 +1,45 @@
 package com.linz.miky.cpsc356
 
+import android.content.Context
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.linz.miky.cpsc356.util.rotate90
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-  private var babyCounter: Int = 0
+  private var babyCounter: Long = 0
+  fun getStore() = getPreferences(Context.MODE_PRIVATE)
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
 
     if (savedInstanceState != null) {
-      babyCounter = savedInstanceState.getInt(BABY_COUNTER_KEY, 0)
+      babyCounter = savedInstanceState.getLong(BABY_COUNTER_KEY, 0)
+      myTextView.text = babyCounter.toString()
     }
 
     myButton.setOnClickListener {
       babyCounter++
+      myTextView.text = babyCounter.toString()
       myImage.rotate90()
+
+      myButton.text = when (babyCounter) {
+        1L -> "stop"
+        in 2 .. 9 -> myButton.text.toString().plus("!")
+        else -> myButton.text
+      }
     }
+  }
+
+  override fun onPause() {
+    super.onPause()
+    getStore().edit().putLong(BABY_COUNTER_KEY, babyCounter).apply()
   }
 
   override fun onSaveInstanceState(outState: Bundle?) {
     outState?.run {
-      putInt(BABY_COUNTER_KEY, babyCounter)
+      putLong(BABY_COUNTER_KEY, babyCounter)
     }
 
     super.onSaveInstanceState(outState)
